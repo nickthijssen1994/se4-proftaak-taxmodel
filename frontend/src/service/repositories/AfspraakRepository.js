3ba42f2235cd874b15c56ff87dd76a5a7e5d039d
Sverre <43172180+Sverrevg@users.noreply.github.com>
2020-03-16 11:26:53 +0100

    Methode commented

--- frontend/src/service/repositories/AfspraakRepository.js
+++ frontend/src/service/repositories/AfspraakRepository.js
@@ -3,12 +3,12 @@ import Repository from "@/service/Repository";
 const resource = "/afspraken"
 
 export default {
-  post(token) {
-    let config = {
-      headers: {Authorization: `Bearer ${token}`}
-    };
-    return Repository.post(`${resource}`, config);
-  }
+  // post(token) {
+  //   let config = {
+  //     headers: {Authorization: `Bearer ${token}`}
+  //   };
+  //   return Repository.post(`${resource}`, config);
+  // }
   getById(id, token) {
     let config = {
       headers: {Authorization: `Bearer ${token}`}
