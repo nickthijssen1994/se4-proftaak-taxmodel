696107dbf537a11af76bff7eb3f953c764c8830c
Sverre <43172180+Sverrevg@users.noreply.github.com>
2020-03-16 11:20:40 +0100

    Repository toegevoegd
    -Opzet voor verbinding met api

---
+++ frontend/src/service/repositories/AfspraakRepository.js
@@ -0,0 +1,18 @@
+import Repository from "@/service/Repository";
+
+const resource = "/afspraken"
+
+export default {
+  post(token) {
+    let config = {
+      headers: {Authorization: `Bearer ${token}`}
+    };
+    return Repository.post(`${resource}`, config);
+  }
+  getById(id, token) {
+    let config = {
+      headers: {Authorization: `Bearer ${token}`}
+    };
+    return Repository.get(`${resource}/{id}`, config);
+  }
+}
