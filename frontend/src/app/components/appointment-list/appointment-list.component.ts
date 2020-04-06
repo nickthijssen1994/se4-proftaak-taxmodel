3b9aad2d5eee0cf4d0013ceadebd444a2d983e0b
Nick Thijssen <nickthijssen1994@gmail.com>
2020-04-06 10:38:22 +0200

    Added Appointment List Component

--- frontend/src/app/components/appointment-list/appointment-list.component.ts
+++ frontend/src/app/components/appointment-list/appointment-list.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Appointment} from '../../models/Appointment';
+import {AppointmentTestService} from '../../services/appointment-test.service';
 
 @Component({
   selector: 'app-appointment-list',
@@ -7,9 +9,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AppointmentListComponent implements OnInit {
 
-  constructor() { }
+  appointments: Appointment[];
+
+  constructor(private appointmentTestService: AppointmentTestService) {
+  }
 
   ngOnInit(): void {
+    this.getAppointments();
+  }
+
+  getAppointments(): void {
+    this.appointmentTestService.getAppointments().subscribe(appointments => this.appointments = appointments);
+    console.log(this.appointments);
+  }
+
+  add(title: string): void {
+    title = title.trim();
+    if (!title) {
+      return;
+    }
+    this.appointmentTestService.addAppointment({title} as Appointment)
+      .subscribe(appointment => {
+        this.appointments.push(appointment);
+      });
+  }
+
+  delete(appointment: Appointment): void {
+    this.appointments = this.appointments.filter(a => a !== appointment);
+    this.appointmentTestService.deleteAppointment(appointment).subscribe();
   }
 
 }
