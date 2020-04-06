224dd64b1f7056566e4c81db95ac09706a15a62b
Nick Thijssen <nickthijssen1994@gmail.com>
2020-04-06 10:21:24 +0200

    Added Test API Service

---
+++ frontend/src/app/services/appointment-test.service.spec.ts
@@ -0,0 +1,16 @@
+import { TestBed } from '@angular/core/testing';
+
+import { AppointmentTestService } from './appointment-test.service';
+
+describe('AppointmentTestService', () => {
+  let service: AppointmentTestService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({});
+    service = TestBed.inject(AppointmentTestService);
+  });
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+});
--- frontend/src/app/services/in-memory-data.service.ts
+++ frontend/src/app/services/in-memory-data.service.ts
@@ -6,23 +6,16 @@ import {Appointment} from '../models/Appointment';
   providedIn: 'root'
 })
 export class InMemoryDataService implements InMemoryDbService {
+
   createDb() {
     const appointments = [
-      { id: 11, name: 'Dr Nice' },
-      { id: 12, name: 'Narco' },
-      { id: 13, name: 'Bombasto' },
-      { id: 14, name: 'Celeritas' },
-      { id: 15, name: 'Magneta' },
-      { id: 16, name: 'RubberMan' },
-      { id: 17, name: 'Dynama' },
-      { id: 18, name: 'Dr IQ' },
-      { id: 19, name: 'Magma' },
-      { id: 20, name: 'Tornado' }
+      { id: 1, title: 'Test1', description: 'Test1', location: 'Test1'},
+      { id: 2, title: 'Test2', description: 'Test2', location: 'Test2'}
     ];
     return {appointments};
   }
 
   genId(appointments: Appointment[]): number {
-    return appointments.length > 0 ? Math.max(...appointments.map(hero => appointments.id)) + 1 : 11;
+    return appointments.length > 0 ? Math.max(...appointments.map(appointment => appointment.id)) + 1 : 1;
   }
 }
