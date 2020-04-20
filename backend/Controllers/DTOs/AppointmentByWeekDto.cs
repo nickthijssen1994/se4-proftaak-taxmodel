bc5fddb65866394ef4d0ab6a0ec3592eabf6120d
Nick Thijssen <nickthijssen1994@gmail.com>
2020-04-20 14:28:39 +0200

    Update AppointmentByWeekDto.cs

--- backend/Controllers/DTOs/AppointmentByWeekDto.cs
+++ backend/Controllers/DTOs/AppointmentByWeekDto.cs
@@ -1,13 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace backend.Controllers.DTOs
 {
-    public class AppointmentByWeekDto
-    {
-        public DateTime BeginTime { get; set; }
-        public DateTime EndTime { get; set; }
-    }
+	public class AppointmentByWeekDto
+	{
+		public DateTime BeginTime { get; set; }
+		public DateTime EndTime { get; set; }
+	}
 }
