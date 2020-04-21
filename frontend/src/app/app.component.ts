a343dd38f710fea3ae3a6e290fde784e038decc1
Job van Roozendaal <j.vanroozendaal@student.fontys.nl>
2020-04-21 13:52:49 +0200

    Agenda modified

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,10 +1,50 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Interaction} from '@fullcalendar/core';
 
+
+declare var $: any;
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'frontend';
+export class AppComponent implements OnInit {
+  title = 'Lunch Calendar';
+  posts: any;
+  constructor(private http: HttpClient) { }
+
+  ngOnInit() {
+    this.getItems();
+  }
+
+  getItems() {
+    // return this.http.get().subscribe(data => {
+   //  this.posts = data
+     setTimeout(() => {
+        $('#calendar').fullCalendar({
+          header: {
+            left: 'prev,next',
+            center: 'month,agendaWeek,agendaDay',
+            right: 'title'
+          },
+          plugins: Interaction,
+          eventClick(info) {
+            alert('Datum = ' + new Date(info.start).toLocaleString() + ' - ' + new Date(info.end).toLocaleString());
+          },
+          navLinks: true,
+          editable: true,
+          eventLimit: true,
+          hiddenDays: [0, 6],
+          startTime: '10:00',
+          endTime: '15:00',
+          events: [{
+            title: 'Lunch',
+            start: '04-21-2020 13:00',
+            end: '04-21-2020 14:00'
+          }]
+        });
+      }, 100);
+    }
 }
+
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,16 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
+import { HttpClientModule } from '@angular/common/http';
+import {FullCalendarModule} from '@fullcalendar/angular';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    HttpClientModule,
+    FullCalendarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
