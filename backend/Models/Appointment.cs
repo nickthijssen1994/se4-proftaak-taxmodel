473c241be19b5e50a91da56b8066b00cb375af6e
Sverre van Gompel <403979@student.fontys.nl>
2020-05-04 14:58:21 +0200

    Added validation to appointment entity

--- backend/Models/Appointment.cs
+++ backend/Models/Appointment.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace backend.Models
@@ -8,10 +9,15 @@ namespace backend.Models
 	public class Appointment
 	{
 		public long Id { get; set; }
+		[Required]
 		public string Title { get; set; }
+		[Required]
 		public DateTime BeginTime { get; set; }
+		[Required]
 		public DateTime EndTime { get; set; }
+		[Required]
 		public string Location { get; set; }
+		[Required]
 		public Account Organiser { get; set; }
 		public ICollection<Order> Orders { get; set; }
 		public string Description { get; set; }
