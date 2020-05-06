3b3bf039111182e70afe31c196febfcd8b7b0691
Sverre van Gompel <403979@student.fontys.nl>
2020-05-06 15:10:59 +0200

    Added minpeople, maxpeople and min length to title and location

--- backend/Models/Appointment.cs
+++ backend/Models/Appointment.cs
@@ -10,18 +10,21 @@ namespace backend.Models
     {
         public long Id { get; set; }
         [Required]
-        [MaxLength(32, ErrorMessage = "Title must be 32 characters or less")]
+        [MaxLength(50, ErrorMessage = "Title must be 32 characters or less."), MinLength(5, ErrorMessage = "Title must be at least 5 characters.")]
         public string Title { get; set; }
         [Required]
         public DateTime BeginTime { get; set; }
         [Required]
         public DateTime EndTime { get; set; }
         [Required]
+        [MaxLength(250, ErrorMessage = "Location must be 32 characters or less."), MinLength(5, ErrorMessage = "Location must be at least 5 characters.")]
         public string Location { get; set; }
+        public int MinPeople { get; set; }
+        public int MaxPeople { get; set; }
         [Required]
         public Account Organiser { get; set; }
         public ICollection<Order> Orders { get; set; }
-        [MaxLength(500, ErrorMessage = "Description must be 500 characters or less")]
+        [MaxLength(250, ErrorMessage = "Description must be 500 characters or less.")]
         public string Description { get; set; }
     }
 }
