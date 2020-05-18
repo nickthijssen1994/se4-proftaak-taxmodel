1a3e685bf68447092e0b209e85f8faa18baa577c
sweerstra <sybweerstra@live.nl>
2020-05-18 10:37:24 +0200

    Added account controller with login and register endpoints

---
+++ backend/Controllers/AccountController.cs
@@ -0,0 +1,64 @@
+﻿using backend.Models.DTOs.Accounts;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace backend.Controllers
+{
+    [ApiController]
+    [Route("taxbreak/api/[controller]")]
+    public class AccountController : ControllerBase
+    {
+        //Inject UserService
+        public AccountController()
+        {
+        }
+
+        [HttpPost("login")]
+        public ActionResult<bool> Login(LoginDto dto)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest();
+            }
+
+            // Auth with identity
+
+            bool loginFailed = false;
+
+            if (!loginFailed)
+            {
+                //TODO: Return something useful
+                return true;
+            }
+            else
+            {
+                return Unauthorized();
+            }
+        }
+
+        [HttpPost("register")]
+        public IActionResult Register(RegisterDto dto)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest();
+            }
+
+            // Register with identity
+
+            bool registerFailed = false;
+
+            if (!registerFailed)
+            {
+                // TODO: return something useful
+                return Ok();
+            }
+            else
+            {
+                return Unauthorized();
+            }
+        }
+    }
+}
---
+++ backend/Models/DTOs/Accounts/RegisterDto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace backend.Models.DTOs.Accounts
+{
+    //TODO: Needs more validation 
+    public class RegisterDto
+    {
+        [Required]
+        public string Email { get; set; }
+        [Required]
+        public string Username { get; set; }
+        [Required]
+        public string Password { get; set; }
+    }
+}
